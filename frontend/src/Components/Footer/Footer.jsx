60b88820b9007a4e6f0783ae59b0400ad4c43594
Eliina0 <152660908+Eliina0@users.noreply.github.com>
2024-08-06 00:14:54 +0200

    Add files via upload

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <hr />
+        <p>Product List Project</p>
+    </div>
+  )
+}
+
+export default Footer;
